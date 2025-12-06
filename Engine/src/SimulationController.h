a1c460fcbe4da6201ba4bdf81eb0f76b5791acaa
Jana Puig Solà <160216332+JanaPuig@users.noreply.github.com>
2025-12-06 13:50:18 +0100

    clean code

--- Engine/src/SimulationController.h
+++ Engine/src/SimulationController.h
@@ -1,34 +1,30 @@
 #pragma once
 
-#include "Module.h" // Si hereda de Module
+#include "Module.h" 
 #include "glm/glm.hpp"
 
-// 1. Definir los posibles estados
 enum class GameState {
-    STOPPED, // Estado inicial / Modo Edición
-    RUNNING, // Modo Juego
-    PAUSED   // Modo Pausa
+    STOPPED, 
+    RUNNING,
+    PAUSED  
 };
 
-class SimulationController : public Module // O simplemente una clase Singleton
+class SimulationController : public Module
 {
 public:
     SimulationController();
     ~SimulationController();
 
-    // 2. Funciones de Control Público
     void Play();
     void Pause();
     void Stop();
 
-    // 3. Getters para el estado y el tiempo
     GameState GetState() const { return currentState; }
     float GetGameDeltaTime(float realDeltaTime) const;
 
 private:
     GameState currentState;
 
-    // 4. Funciones de Coordinación
     void SaveInitialSceneState();
     void LoadInitialSceneState();
 };
